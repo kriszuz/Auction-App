22cb6ed960d6cc627cba9afc0cda86bd0867fcfe
kriszuz <109152045+kriszuz@users.noreply.github.com>
2023-01-23 12:37:56 +0100

    Added functionality to hamburger menu in navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { BiMenuAltRight } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const [responsiveNavOpen, setResponsiveNavOpen] = useState(false)
+
+  const closeNav = () => {
+    if (responsiveNavOpen){
+      setResponsiveNavOpen(false)
+    }
+  }
+  
   return (
     <nav className='container'>
         <span><Link to="/">LICYTACJA</Link ></span>
-        <Link to="/gra" className='nav-link'>Graj</Link>
-        <Link to="/jakgrac" className='nav-link'>Jak grać?</Link>
-        <Link to="/tworca" className='nav-link'>Twórca</Link>
-        <BiMenuAltRight className='hamburger'/>
+         <div className={responsiveNavOpen ? "nav-links-container active" : "nav-links-container"}>
+            <Link to="/gra" className='nav-link' onClick={() => closeNav()}>Graj</Link>
+            <Link to="/jakgrac" className='nav-link' onClick={() => closeNav()}>Jak grać?</Link>
+            <Link to="/tworca" className='nav-link' onClick={() => closeNav()}>Twórca</Link>
+         </div >
+        <BiMenuAltRight className='hamburger' onClick={() => setResponsiveNavOpen(!responsiveNavOpen)}/>
     </nav>
   )
 }
