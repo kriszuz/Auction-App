f0a93258d3c111b4252829b9f6faecc0af1c8e98
kriszuz <109152045+kriszuz@users.noreply.github.com>
2023-01-24 17:52:08 +0100

    Created howToPlay page with game rules

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Hero from './pages/Hero'
 import Footer from './components/Footer'
 import Game from './pages/Game'
 import About from './pages/About'
+import HowToPlay from './pages/HowToPlay'
 
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
         {/* pages */}
         <Route path='/' element={<Hero />}/>
         <Route path='/gra' element={<Game />}/>
-        <Route path='/jakgrac' element={<h1>jak nalezy grac</h1>}/>
+        <Route path='/jakgrac' element={<HowToPlay />}/>
         <Route path='/tworca' element={<About /> }/>
         {/* 404 page */}
         <Route path="*" element={<h1>404</h1>} /> 
--- src/pages/HowToPlay.tsx
+++ src/pages/HowToPlay.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const HowToPlay = () => {
   return (
-    <div>HowToPlay</div>
+    <div className='container rules-page-container'>
+    <h2 className='rules-h2'>Zasady Gry</h2>
+    <div className='rules-container'>
+      <ol>
+        <li>Gracze wspólnie losują kategorię, w której będą licytować.</li>
+        <li>Gracze licytują ile potrafią wymienić przedmiotów z wylosowanej kategorii.</li>
+        <li>Gracz, który wygrał licytacje i tym samym zadeklarował się że zna najwięcej 
+            przedmiotów z wylosowanej kategorii ma 60 sekund na ich wymienienie.</li>
+        <li>Jeśli gracz, który wygrał licytacje zdoła wymienić zadeklarowaną ilość 
+            przedmiotów z wylosowanej kategorii otrzymuje 1 punkt*.</li>
+      </ol>
+      <p>*Gracze liczą punkty we własnym zakresie, aplikacja służy jedynie do losowania kategorii, zobaczenia
+          przykładowych odpowiedzi, a także do odmierzenia czasu. Ilość graczy jest dowolna.</p>
+    </div>
+    </div>
   )
 }
 
