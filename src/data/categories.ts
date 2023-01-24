3013cbafa87f6ab3b01ffd7bce53d0e5e2f65a7d
kriszuz <109152045+kriszuz@users.noreply.github.com>
2023-01-24 20:03:43 +0100

    added faviicon and some categories in data file

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,57 +3,59 @@ export default {
       {
         name: 'Miasta w Polsce na literę A',
         items: ['Abramów', 'Adamów', 'Adamówka', 'Aleksandrów', 'Aleksandrów Kujawski', 'Aleksandrów Łódzki', 'Alwernia', 'Andrespol', 'Andrychów', 'Andrzejewo', "Annopol", "Augustów"],
-        description: 'Istnieje 14 miast na litere A'
+        description: 'Istnieje 14 miast na literę A'
       },
       {
         name: 'Miasta w Polsce na literę B',
         items: ["Bydgoszcz", "Bielsko-Biała", "Białystok", "Bartoszyce", "Bolesławiec", "Bełchatów", "Białogard", "Biała Podlaska", "Barczewo", "Bielawa", "Białogard"],
-        description: 'Istnieje sporo miast na litere B'
+        description: 'Istnieje sporo miast na literę B'
       },
       {
         name: 'Kraje graniczace z Polska',
         items: ["Niemcy", "Czechy", "Słowacja", "Ukraina", "Białoruś", "Litwa", "Rosja", "Słowacja"],
-        description: 'Polska graniczy z siedmioma panstwami'
+        description: 'Polska graniczy z siedmioma państwami'
       },
       {
         name: 'Kraje Europy na literę B',
         items: ["Belgia", "Bośnia i Hercegowina", "Bułgaria", "Białoruś"],
-        description: 'W Europie sa 4 kraje na litere B'
+        description: 'W Europie są 4 kraje na literę B'
       },
       {
         name: 'Samochody marki Ford',
         items: ['Mustang', 'Fiesta', 'Focus', 'Mondeo', 'Kuga', 'EcoSport', "Galaxy", "Explorer", "Ranger", "Transit", "S-Max"],
-        description: 'Ford wyprodukowal ponad 100 roznych modeli aut'
+        description: 'Ford wyprodukował ponad 100 roznych modeli aut'
       },
       {
         name: 'Marki napojów gazowanych',
         items: ["Coca-Cola", "Pepsi", "Sprite", "Fanta", "7Up", "Mountain Dew", "Dr Pepper", "Schweppes", "Kinley", "Burn", "Red Bull", "Mirinda"],
-        description: "Napoje tego typu sa slodzone i szkodza zdrowiu, jesli ich nie znacie to dobrze"
+        description: "Napoje tego typu są słodzone i szkodzą zdrowiu, jeśli ich nie znacie to dobrze"
       },
       {
         name: 'Zwierzęta domowe',
         items: ['Pies', 'Kot', 'Królik', 'Chomik', 'Kanarek', 'Papuga', "Świnka morska", "Myszka", "Szynszyla", "Żółw", "Rybki", "Koszatniczka", "Jaszczurka", "Wąż", "Pająk"],
-        description: 'W polskich domach łącznie mieszka okolo 21 mln zwierzat'
+        description: 'W polskich domach łącznie mieszka około 21 mln zwierząt'
       },
       {
         name: 'Warzywa',
         items: ['Marchew', 'Pomidor', 'Cebula', 'Ziemniak', 'Papryka', 'Seler'],
-        description: 'Najpopularniejsze wazywo w polsce to ogórek'
+        description: 'Najpopularniejsze warzywo w Polsce to ogórek'
       },
       {
         name: 'Owoce',
         items: ["Jabłko", "Banan", "Mango", "Ananas", "Kiwi", "Pomarańcza", "Grejpfrut", "Cytryna", "Winogrono", "Durian", "Avocado", "Granat", "Liczi", "Tangerin", "Czerwona porzeczka", "Malina", "Brzoskwinia", "Truskawka", "Jagoda", "Morela", "Gruszka", "Borówka"],
-        description: 'Liczba gatunkow owocow na swiecie moze wynosić nawet do kilku dziesiatek tysiecy'
+        description: 'Liczba gatunków owoców na świecie może wynosić nawet do kilku dziesiątek tysięcy'
       },
       {
-        name: 'Panstwa Europy',
+        name: 'Państwa Europy',
         items: ["Albania", "Andora", "Austria", "Belgia", "Białoruś", "Bośnia i Hercegowina", "Bułgaria", "Chorwacja", "Cypr", "Czechy", "Dania", "Estonia", "Finlandia", "Francja", "Grecja", "Hiszpania", "Holandia", "Irlandia", "Islandia", "Kazachstan", "Litwa", "Luksemburg", "Łotwa", "Malta", "Niemcy", "Norwegia", "Polska", "Portugalia", "Rumunia", "Rosja", "Słowacja", "Słowenia", "Szwajcaria", "Szwecja", "Turcja", "Ukraina", "Węgry", "Włochy", "Wielka Brytania"],
         description: 'Na terenie Europy znajduje się 46 państw'
       },
-        // {
-        // name: 'Marki smartfonów',
-        // items: ['Samsung', 'Apple', 'Xiaomi', 'Huawei', 'OnePlus', 'LG']
-        // },
+      {
+        name: 'Marki smartfonów',
+        items: ['Samsung', 'Apple', 'Xiaomi', 'Huawei', 'OnePlus', 'LG', 'Sony', 'Honor', 'Lenovo', 'HTC'],
+        description: 'Najpopularniejszą marką smartfona w Polsce jest Samsung'
+      },
+    
         // {
         // name: 'Marki samochodów',
         // items: ['BMW', 'Mercedes-Benz', 'Audi', 'Volkswagen', 'Toyota', 'Ford']
