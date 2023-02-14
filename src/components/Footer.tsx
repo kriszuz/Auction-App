cebda685a9eb1190b6ebc6dddb1a5596aef7720c
kriszuz <109152045+kriszuz@users.noreply.github.com>
2023-02-14 20:37:19 +0100

    added anchor tag in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer>made with <FaHeart fill='#8C512F'/> by kriszuz</footer>
+    <footer>made with <FaHeart fill='#8C512F'/> by <a target="_blank" href="https://www.linkedin.com/in/krzysztof--marciniak/">kriszuz</a></footer>
   )
 }
 
